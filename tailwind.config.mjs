94557d5765b8faec1db32e569039ec6eea1daed6
Archie <archie@veed.io>
2024-03-30 17:51:10 +0000

    theme colors, footer subscribe

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.stone,
+        border: colors.stone[200],
+        primary: colors.stone[900],
+        secondary: colors.stone[500],
+        background: colors.stone[100],
+        accent: colors.orange,
+      },
+    },
   },
   plugins: [],
 };
